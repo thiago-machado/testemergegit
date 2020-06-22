365b07e39a97d827775d0f51dafa741c4aea9786
Thiago Machado <ti-thiago@outlook.com>
2020-06-22 01:52:13 -0300

    Alteração no HTML

--- src/java/ClassePrincipal.java
+++ src/java/ClassePrincipal.java
@@ -3,6 +3,8 @@ package br.com.teste;
 public class ClassePrincipal {
 	public static void main (String[] args) {
 		
+		System.out.println("Bem vindo ao sistema...");
+		
 		if(args != null && args.size() > 0) {
 			for(String argument : args) {
 				System.out.println(argument);
