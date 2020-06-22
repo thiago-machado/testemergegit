6ecff27111dc9dd31d4942083869d0449316f6d9
Thiago Machado <ti-thiago@outlook.com>
2020-06-22 01:02:34 -0300

    Edição nos arquivos html e java

--- src/java/ClassePrincipal.java
+++ src/java/ClassePrincipal.java
@@ -2,6 +2,13 @@ package br.com.teste;
 
 public class ClassePrincipal {
 	public static void main (String[] args) {
-		System.out.println(args);
+		
+		if(args != null && args.size() > 0) {
+			for(String argument : args) {
+				System.out.println(argument);
+			}
+		} else {
+			System.err.println("Without arguments...");
+		}
 	}
 }
